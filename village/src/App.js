096f5d2e6f6a93ed7adcd12d440c12e26f2d7756
Angela Williams <angel_legato@icloud.com>
2019-06-30 21:05:29 -0400

    Browser Routing Completed

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
@@ -16,7 +17,6 @@ class App extends Component {
     axios.get('http://localhost:3333/smurfs').then(response => this.setState({smurfs : response.data})).catch(error => console.log(error));
   }
 
-  
   reloadSmurfs = (newSmurfs) => {
     this.setState({
       smurfs: newSmurfs
@@ -25,10 +25,20 @@ class App extends Component {
   
   render() {
     return (
+      <Router>
       <div className="App">
-        <SmurfForm reloadSmurfs={this.reloadSmurfs}/>
-        <Smurfs smurfs={this.state.smurfs} />
+      
+      <div className = 'nav-bar'>
+      <NavLink to = '/'>Smurfs</NavLink>
+      <NavLink to = '/smurf-form'>Add Smurfs</NavLink>
       </div>
+      
+      <Route exact path = '/' render = {(props) => <Smurfs {...props} smurfs = {this.state.smurfs} />} />
+      <Route path = '/smurf-form' render = {(props) => <SmurfForm {...props} reloadSmurfs = {this.reloadSmurfs} />} />
+      
+      
+      </div>
+      </Router>
     );
   }
 }
